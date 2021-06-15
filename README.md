d24a376e507722f8b21116fa492cf45320109e09
peter-duong-tgl <peter.duong@tgl.co>
2021-06-15 14:07:15 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ngrx-example-hcg1of
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ngrx-example-hcg1of)
---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { booksReducer } from './state/books.reducer';
+import { collectionReducer } from './state/collection.reducer';
+import { StoreModule } from '@ngrx/store';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    StoreModule.forRoot({ books: booksReducer, collection: collectionReducer })
+  ],
+  declarations: [AppComponent],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
+
+/*
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://github.com/ngrx/platform
+*/
